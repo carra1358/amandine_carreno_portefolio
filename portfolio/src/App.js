9e47a13cdf88b4571a4dc467629df928c021eaa3
amandine carreno <30933638+carra1358@users.noreply.github.com>
2022-07-08 19:15:37 +0200

    data completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,9 @@
 import Header from './components/header/Header';
+import { data } from "./data/data"
 import './app.css';
 import Bio from './components/bio/Bio';
 import profilePic from "./assets/profile_pic.jpg"
+import Main from './components/main/Main';
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       <Header />
       <main>
         <Bio src={profilePic} />
-
+        <Main data={data} />
       </main>
     </div>
 
---
+++ portfolio/src/data/data.js
@@ -0,0 +1,144 @@
+export const data = [
+
+    {
+        type: "projects",
+        id: "pr1",
+        title: "Convert a jquery website in React and create your own library",
+        description: ["Analyse a web app's performance", "Deploy a front-end app", "Code in functional javascript", "Maximize web app performance"],
+        deploy: true,
+        link: "https://amandine-carreno-p14-02-05-2022.vercel.app/",
+        github: "https://github.com/carra1358/amandine_carreno_p14_02_05_2022",
+        cover: "gs://carra-portfolio-assets.appspot.com/p14_cover.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/p14_cover.png"]
+    },
+    {
+        type: "projects",
+        id: "pr2",
+        title: "Build a react app using an API for users banking dashboard",
+        description: ["Implement react's state management", "Interact with an API", "Conseive an API endpoints", "Create an authentification system"],
+        deploy: false,
+        link: "",
+        github: "https://github.com/carra1358/amandine_carreno_13_080422/tree/main/argent_bank",
+        cover: "gs://carra-portfolio-assets.appspot.com/p13_cover.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/p13_cover.png"]
+    },
+    {
+        type: "projects",
+        id: "pr3",
+        title: "Build an analytics dashboard with React",
+        description: ["Ensuring the data quality of an application", "Interact with a web service", "Develop advanced graphical elements using JavaScript libraries", "Produce technical documentation for an application"],
+        deploy: false,
+        link: "",
+        github: "https://github.com/carra1358/amandine_carreno_p12_030322",
+        cover: "gs://carra-portfolio-assets.appspot.com/p12_cover.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/p12_cover.png"]
+    },
+    {
+        type: "projects",
+        id: "pr4",
+        title: "Build an app using React App and React Router",
+        description: ["Create routes with reactRouter", "Start a project with a framework", "Create components"],
+        deploy: true,
+        link: "https://6220ae151fdc41c1146c4cf5--competent-elion-88a1e3.netlify.app/",
+        github: "https://github.com/carra1358/amandine_carreno_p11_kasa_06_02_22",
+        cover: "gs://carra-portfolio-assets.appspot.com/p11_cover.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/p11_cover.png"]
+    },
+    {
+        type: "projects",
+        id: "pr4",
+        title: "Montse Rocabert Art",
+        description: ["Personnal project realised with wordpress for a catalan artist"],
+        deploy: true,
+        link: "https://www.montserocabert.com/",
+        github: "",
+        cover: "gs://carra-portfolio-assets.appspot.com/pp1.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/pp1.png"]
+    },
+    {
+        type: "projects",
+        id: "pr6",
+        title: "Create two search algorithms",
+        description: ["Analyse and solve a problem with javascript"],
+        deploy: true,
+        link: "https://carra1358.github.io/amandine_carreno_7_12072021/",
+        github: "https://github.com/carra1358/amandine_carreno_7_12072021",
+        cover: "gs://carra-portfolio-assets.appspot.com/p7_cover.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/p7_cover.png"]
+    },
+    {
+        type: "skills",
+        id: "sk1",
+        title: "React",
+        description: "During my cursus, I was introduced to React and fell in love with it.Here is the link of my last project made with React https://amandine-carreno-p14-02-05-2022.vercel.app/ ",
+    },
+    {
+        type: "skills",
+        id: "sk2",
+        title: "Javascript",
+        description: "Understanding JavaScript has been the hardest part of my learning journey. Between the DOM, the algorithm and object-oriented programming, I was completely lost. Thanks to my perseverance and hard work, I succeeded in this challenge.",
+    },
+    {
+        type: "skills",
+        id: "sk3",
+        title: "Agile",
+        description: "I understood the agile methodology through the creation of user stories, Kanban boards and test scenarios.",
+    },
+    {
+        type: "skills",
+        id: "sk4",
+        title: "API REST",
+        description: "during my course all my calls were made with the Rest method. My next challenge is to master GraphSQL",
+    },
+    {
+        type: "skills",
+        id: "sk5",
+        title: "HTML/CSS",
+        description: "My first `Hello World` in red font felt like magic",
+    },
+    {
+        type: "skills",
+        id: "sk6",
+        title: "Unit testing",
+        description: "Unit testing is essential to the reliability and maintenance of an application. But I must say that it is quite tedious at first.",
+    },
+    {
+        type: "skills",
+        id: "sk6",
+        title: "JS Doc",
+        description: "Being able to make your code understandable and reusable is one of the skills I value the most as I learned so much from the shared resources and code.",
+    },
+    {
+        type: "education",
+        id: "ed0",
+        title: "Bachelor Bi disciplinary Communication and Politics",
+        description: "Right after graduating from high school, I crossed the Atlantic Ocean and studied at the University of Montreal. It was a great experience to develop my autonomy, my open-mindedness, my resilience and my personal accomplishment.",
+        cover: "gs://carra-portfolio-assets.appspot.com/udem_grad.jpg",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/udem_grad.jpg"]
+    },
+    {
+        type: "education",
+        id: "ed1",
+        title: "Certification Front-End developer",
+        description: "Completing the OpenClassroom Front-End Developer Curriculum allowed me to take my first steps towards my new career. From today I feel confident to be a real asset for my future team.",
+        cover: "gs://carra-portfolio-assets.appspot.com/opc.png",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/opc.png"]
+    },
+    {
+        type: "interests",
+        id: "in1",
+        title: "Travel",
+        description: "From Lisbon to Seoul and more, I enjoyed all the time spent learning about new cultures, languages ​​and architectures.",
+        cover: "gs://carra-portfolio-assets.appspot.com/ptg_travel.jpg",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/ptg_travel.jpg", "gs://carra-portfolio-assets.appspot.com/kr_travel.JPG", "gs://carra-portfolio-assets.appspot.com/jpn_travel.jpg"]
+    }, {
+        type: "interests",
+        id: "in2",
+        title: "Gastronomy",
+        description: "I live to share the food I've cooked with others and discover new recipes",
+        cover: "gs://carra-portfolio-assets.appspot.com/straberry_shortcake.jpg",
+        pictures: ["gs://carra-portfolio-assets.appspot.com/straberry_shortcake.jpg", "gs://carra-portfolio-assets.appspot.com/tako_food.jpg"]
+    },
+
+]
+
